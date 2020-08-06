f87d69dea8533c7639c8f189f2dc6b4f801940dc
dmryakup <65350376+dmryakup@users.noreply.github.com>
2020-08-06 13:55:05 +0300

    Add files via upload

---
+++ PredictionOfClothing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PredictionOfClothingML.Model;
+
+namespace PredictionOfClothing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = new ModelInput();
+            //ModelOutput result = ConsumeModel.Predict(input);
+        }
+    }
+}
